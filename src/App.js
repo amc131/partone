03e95440539cad66a5fb11b301dc36b57f925980
amc131 <76466256+amc131@users.noreply.github.com>
2022-06-06 12:01:37 -0700

    Updated for part 1.2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part = {props.part1} exercises = {props.exercises1}/>
+      <Part part = {props.part2} exercises = {props.exercises2}/>
+      <Part part = {props.part3} exercises = {props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+ return (
+   <p>
+     Number of Exercises {props.exercise1 + props.exercise2 + props.exercise3}
+   </p>
+ )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,6 +39,24 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content 
+        part1 = {part1}
+        exercises1={exercises1}
+        part2 = {part2}
+        exercises2={exercises2}
+        part3 = {part3}
+        exercises3 = {exercises3}
+      />
+      <Total 
+        exercise1 = {exercises1}
+        exercise2 = {exercises2}
+        exercise3 = {exercises3}
+      />
+    </div>
+  )
 }
 
 export default App;
